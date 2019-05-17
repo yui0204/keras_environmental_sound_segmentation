677149f80a133cdf9d7036994478ea905cadd080
yui0204 <yui.sudo@gmail.com>
2019-05-17 18:56:09 +0900

    change dataset noise level

--- main.py
+++ main.py
@@ -784,9 +784,9 @@ if __name__ == '__main__':
     else:
         datasets_dir = "/misc/export2/sudou/sound_data/datasets/"
     
-    for datadir in ["multi_segdata"+str(classes) + "_"+str(image_size)+"_no_sound/", 
+    for datadir in [#"multi_segdata"+str(classes) + "_"+str(image_size)+"_no_sound/", 
                     #"multi_segdata"+str(classes) + "_"+str(image_size)+"_-30dB/", 
-                    #"multi_segdata"+str(classes) + "_"+str(image_size)+"_-20dB/", 
+                    "multi_segdata"+str(classes) + "_"+str(image_size)+"_-20dB/", 
                     #"multi_segdata"+str(classes) + "_"+str(image_size)+"_-10dB/", 
                     #"multi_segdata"+str(classes) + "_"+str(image_size)+"_0dB/"
                     ]:
