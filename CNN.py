dfa06b7a52691fa923f3c80db781b280047d0e4a
yui0204 <yui.sudo@gmail.com>
2019-09-16 22:55:11 +0900

    DropoutとBatchnormを併用してはいけないようなので修正

--- CNN.py
+++ CNN.py
@@ -15,26 +15,21 @@ def CNN4(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
 
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
             
     x = Conv2D(n_classes, (1, 1), activation='sigmoid')(x)
-    #x = core.Activation('softmax')(x)
     
     model = Model(inputs=inputs, outputs=x)
     
@@ -46,46 +41,37 @@ def CNN8(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
             
     x = Conv2D(n_classes, (1, 1), activation='sigmoid')(x)
-    #x = core.Activation('softmax')(x)
     
     model = Model(inputs=inputs, outputs=x)
     
@@ -97,40 +83,33 @@ def CRNN(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
 
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 1), strides=(4, 1))(x) # ?, 1, 512, 512
 
     
     x = Reshape((input_width, 512))(x)
         
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     
@@ -147,60 +126,49 @@ def CRNN8(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     
     x = Reshape((input_width, 512))(x)
         
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     
@@ -217,60 +185,49 @@ def BiCRNN8(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     
     x = Reshape((input_width, 512))(x)
         
     x = Bidirectional(GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False))(x) 
+            return_sequences=True, stateful=False))(x) 
     x = BatchNormalization()(x)
 
     x = Bidirectional(GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False))(x) 
+            return_sequences=True, stateful=False))(x) 
     x = BatchNormalization()(x)
 
     x = Bidirectional(GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False))(x) 
+            return_sequences=True, stateful=False))(x) 
     x = BatchNormalization()(x)
 
     
@@ -287,23 +244,19 @@ def RNN(n_classes, input_height=256, input_width=512, nChannels=3):
     x = Reshape((input_width, input_height))(inputs)
         
     x = GRU(300, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
     
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     
@@ -321,18 +274,15 @@ def BiRNN(n_classes, input_height=256, input_width=512, nChannels=3):
     x = Reshape((input_width, input_height))(inputs)
         
     x = Bidirectional(GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False))(x) 
+            return_sequences=True, stateful=False))(x) 
     x = BatchNormalization()(x)
 
     x = Bidirectional(GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False))(x) 
+            return_sequences=True, stateful=False))(x) 
     x = BatchNormalization()(x)
 
     x = Bidirectional(GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False))(x) 
+            return_sequences=True, stateful=False))(x) 
     x = BatchNormalization()(x)
 
     
@@ -349,39 +299,31 @@ def Unet(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 4), strides=(4, 4))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 4), strides=(4, 4))(x)
 
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 4), strides=(4, 4))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((4, 4), strides=(4, 4))(x)
     
     x = Conv2DTranspose(512, kernel_size=(1, 4), strides=(1, 4), activation='relu')(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
 
     x = Conv2DTranspose(256, kernel_size=(1, 4), strides=(1, 4), activation='relu')(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
 
     x = Conv2DTranspose(128, kernel_size=(1, 4), strides=(1, 4), activation='relu')(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
 
     x = Conv2DTranspose(64, kernel_size=(1, 4), strides=(1, 4), activation='relu')(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     
     x = Conv2D(n_classes, (1, 1), activation='sigmoid')(x)    
     
@@ -395,60 +337,49 @@ def Double_CRNN8(n_classes, input_height=256, input_width=512, nChannels=3):
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
     
     x = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(x)    
     x = BatchNormalization()(x)
-    x = Dropout(0.25)(x)
     x = MaxPooling2D((2, 1), strides=(2, 1))(x)
 
     
     x = Reshape((input_width, 512))(x)
         
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     x = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(x) 
+            return_sequences=True, stateful=False)(x) 
     x = BatchNormalization()(x)
 
     
@@ -461,60 +392,49 @@ def Double_CRNN8(n_classes, input_height=256, input_width=512, nChannels=3):
     
     f = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(inputs)
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     f = Conv2D(64, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     f = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)    
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
 
     f = Conv2D(128, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)    
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     f = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     f = Conv2D(256, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     f = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)    
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     f = Conv2D(512, (3, 3), activation='relu', padding='same', dilation_rate=1)(f)    
     f = BatchNormalization()(f)
-    f = Dropout(0.25)(f)
     f = MaxPooling2D((1, 2), strides=(1, 2))(f)
     
     
     f = Reshape((input_width, 512))(f)
         
     f = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(f) 
+            return_sequences=True, stateful=False)(f) 
     f = BatchNormalization()(f)
 
     f = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(f) 
+            return_sequences=True, stateful=False)(f) 
     f = BatchNormalization()(f)
 
     f = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
-            return_sequences=True,
-            dropout=0.25, recurrent_dropout=0.25, stateful=False)(f) 
+            return_sequences=True, stateful=False)(f) 
     f = BatchNormalization()(f)
 
     
