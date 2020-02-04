c0e99ce3542fd9b1cb25f1a201d96dd1bd740c62
yui0204 <yui.sudo@gmail.com>
2020-02-04 18:35:58 +0900

    ２段めのU-Netはmultiplyを廃止

--- Unet.py
+++ Unet.py
@@ -176,15 +176,87 @@ def WNet(n_classes, input_height=256, input_width=512, nChannels=1,
     
     x = sss_model.output
     x = concatenate([sss_model.input[1], x], axis=-1) # concatenate mixes spectrogram
+    e1 = x
+
+    if freq_pool == True:
+        stride = (2, 1)
+    else:
+        stride = (2, 2)
+        
+    e1 = Conv2D(64, (3, 3), strides=stride, padding='same')(e1)
+    e1 = BatchNormalization()(e1)
+    e1 = LeakyReLU(0.2)(e1)
+
+    e2 = Conv2D(128, (3, 3), strides=stride, padding='same')(e1)
+    e2 = BatchNormalization()(e2)
+    e2 = LeakyReLU(0.2)(e2)
     
-    unet = UNet(n_classes = n_classes,  # number of classes
-                input_height=256, input_width=input_width, 
-                nChannels = ang_reso + 1, # number of direction resoluton
-                trainable=trainable, sed_model=sed_model, num_layer=num_layer, 
-                aux=aux, mask=mask, RNN=RNN, freq_pool=freq_pool)
+    e3 = Conv2D(256, (3, 3), strides=stride, padding='same')(e2)
+    e3 = BatchNormalization()(e3)
+    e3 = LeakyReLU(0.2)(e3)
     
-    model = Model(inputs=[sss_model.input[0], sss_model.input[1]], outputs=unet([x, sss_model.input[1]]))
+    e4 = Conv2D(512, (3, 3), strides=stride, padding='same')(e3)
+    e4 = BatchNormalization()(e4)
+    e4 = LeakyReLU(0.2)(e4)
     
+    e5 = Conv2D(512, (3, 3), strides=stride, padding='same')(e4)
+    e5 = BatchNormalization()(e5)
+    e5 = LeakyReLU(0.2)(e5)
+    
+    e6 = Conv2D(512, (3, 3), strides=stride, padding='same')(e5)
+    e6 = BatchNormalization()(e6)    
+    e6 = LeakyReLU(0.2)(e6)  # 4
+    
+    
+    if RNN > 0:
+        e6 = Reshape((-1, 512))(e6)
+        
+        for i in range(RNN):
+            e6 = GRU(512, activation='tanh', recurrent_activation='hard_sigmoid', 
+                    return_sequences=True, stateful=False)(e6) 
+            #e6 = BatchNormalization()(e6)
+        
+        e6 = Reshape((4, -1, 512))(e6)
+    
+    d5 = Conv2DTranspose(512, (3, 3), strides=stride, use_bias=False, 
+                         kernel_initializer='he_uniform', padding='same')(e6)
+    d5 = BatchNormalization()(d5)
+    d5 = Activation('relu')(d5)
+    d5 = Dropout(0.5)(d5)
+    d5 = concatenate([d5, e5], axis=-1)
+    
+    d4 = Conv2DTranspose(512, (3, 3), strides=stride, use_bias=False, 
+                        kernel_initializer='he_uniform', padding='same')(d5)
+    d4 = BatchNormalization()(d4)
+    d4 = Activation('relu')(d4)
+    d4 = Dropout(0.5)(d4)
+    d4 = concatenate([d4, e4], axis=-1)
+
+    d3 = Conv2DTranspose(256, (3, 3), strides=stride, use_bias=False, 
+                        kernel_initializer='he_uniform', padding='same')(d4)
+    d3 = BatchNormalization()(d3)
+    d3 = Activation('relu')(d3)
+    d3 = Dropout(0.5)(d3)
+    d3 = concatenate([d3, e3], axis=-1)
+
+    d2 = Conv2DTranspose(128, (3, 3), strides=stride, use_bias=False, 
+                        kernel_initializer='he_uniform', padding='same')(d3)
+    d2 = BatchNormalization()(d2)
+    d2 = Activation('relu')(d2)
+    d2 = concatenate([d2, e2], axis=-1)
+
+    d1 = Conv2DTranspose(64, (3, 3), strides=stride, use_bias=False, 
+                        kernel_initializer='he_uniform', padding='same')(d2)
+    d1 = BatchNormalization()(d1)
+    d1 = Activation('relu')(d1)
+    d1 = concatenate([d1, e1], axis=-1)
+    
+    d0 = Conv2DTranspose(n_classes, (3, 3), strides=stride, use_bias=False, 
+                         activation='sigmoid',
+                         kernel_initializer='he_uniform', padding='same')(d1)
+        
+    #d0 = multiply([sss_model.input[1], d0])
+    model = Model(input=[sss_model.input[0], sss_model.input[1]], output=d0)
                         
     return model
 
